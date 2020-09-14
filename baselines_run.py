788415e00c7847839583bf59408f42560b0631ae
Klukov <piotras.kluk@gmail.com>
2020-09-15 00:53:08 +0200

    dump commit

--- baselines_run.py
+++ baselines_run.py
@@ -1,36 +1,58 @@
-from rl_project.algorithm import ppo2
-from rl_project import environment
-
-import logging
 import re
 import time
 
+from absl import app
+from absl import flags
+from absl import logging as logger
+from stable_baselines.common import ActorCriticRLModel
+from stable_baselines.common.vec_env import SubprocVecEnv
+
+from rl_project import environment
+from rl_project.algorithm import ppo2
 from rl_project.environment import SCENARIO_MAP
 
-if __name__ == '__main__':
-    scenario_number = int(17)
-    algorithm = 'ppo2'
-    number_of_envs = int(8)
-    total_number_of_steps = int(1e5)
+FLAGS = flags.FLAGS
 
+flags.DEFINE_integer('scenario_number', int(17),
+                     'Defines scenario number - look at Readme.md')
+flags.DEFINE_enum('algorithm', 'ppo2', ['ppo2'],
+                  'Algorithm to be used for training - only some algorithms from stable-baselines')
+flags.DEFINE_integer('number_of_envs', int(8),
+                     'Number of env run parallel to get result')
+flags.DEFINE_integer('number_of_steps', int(1e5),
+                     'Total number of steps of the algorithm')
+
+
+def main(_):
     run_name = "{algorithm}_scenario-{scenario}_{steps}M-steps_{envs}-env".format(
-        algorithm=algorithm,
-        scenario=scenario_number,
-        steps=str(total_number_of_steps / 1e6).replace(".", "") if total_number_of_steps < 1e6
-                else re.sub("\..*", "", str(total_number_of_steps / 1e6)),
-        envs=number_of_envs,
+        algorithm=FLAGS.algorithm,
+        scenario=FLAGS.scenario_number,
+        steps=str(FLAGS.number_of_steps / 1e6).replace(".", "") if FLAGS.number_of_steps < 1e6
+        else re.sub("\..*", "", str(FLAGS.number_of_steps / 1e6)),
+        envs=FLAGS.number_of_envs,
     )
-    logging.basicConfig(filename=run_name + ".log", level=logging.DEBUG)
-    logger = logging.getLogger(run_name)
+    logger.get_absl_handler().use_absl_log_file(run_name, './')
     logger.info("STARTED")
-
     time_start = time.time()
     env = environment.create_training_env(
-        number_of_processes=number_of_envs,
-        level=SCENARIO_MAP.get(scenario_number, 'academy_empty_goal_close'),
+        number_of_processes=FLAGS.number_of_envs,
+        level=SCENARIO_MAP.get(FLAGS.scenario_number, 'academy_empty_goal_close'),
     )
-    trained_model = ppo2.learn_ppo2(vec_env=env, total_number_of_steps=total_number_of_steps)
+    trained_model = train(vec_env=env, total_number_of_steps=FLAGS.number_of_steps, algorithm_name=FLAGS.algorithm)
     time_elapsed = time.time() - time_start
     logger.info("Time elapsed " + time.strftime("%H:%M:%S", time.gmtime(time_elapsed)))
-
     trained_model.save(save_path=run_name)
+
+
+def train(
+        vec_env: SubprocVecEnv,
+        total_number_of_steps: int,
+        algorithm_name: str,
+) -> ActorCriticRLModel:
+    if algorithm_name == 'ppo2':
+        return ppo2.learn_ppo2(vec_env=vec_env, total_number_of_steps=total_number_of_steps)
+    return None
+
+
+if __name__ == '__main__':
+    app.run(main)
