ebc4f3ac770e5b56654168479890aa6f9f0034fb
Klukov <piotras.kluk@gmail.com>
2020-11-09 01:01:05 +0100

    td3 corrections

--- rl_project/algorithm/td3.py
+++ rl_project/algorithm/td3.py
@@ -1,4 +1,7 @@
 from stable_baselines import TD3
+from stable_baselines.common.noise import NormalActionNoise
+import numpy as np
+from stable_baselines.td3 import CnnPolicy
 
 from rl_project.environment import create_training_env
 
@@ -21,6 +24,12 @@ def learn_td3(
     """
     if env is None:
         env = create_training_env(1)
+
+    # The noise objects for TD3
+    # n_actions = getattr(env.action_space, 'n')
+    n_actions = env.action_space.n
+    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
+
     td3 = TD3(
         policy=policy,
         env=env,
@@ -33,9 +42,10 @@ def learn_td3(
         batch_size=batch_size,
         tau=tau,
         policy_delay=2,
-        action_noise=None,
+        action_noise=action_noise,
         target_policy_noise=0.2,
         target_noise_clip=0.5,
         random_exploration=0.0,
+        verbose=2,
     )
     return td3.learn(total_number_of_steps)
